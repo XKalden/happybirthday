85d162bdcaca1d8d5740b0bfbad463a7fa4ef25a
XKalden <kaldend.tsering@outlook.com>
2018-07-27 13:07:01 -0400

    added sound

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 import MainPage from './components/mainPage/Mainpage'; 
 import {Route, Switch} from 'react-router-dom';
 
+
+import Rikkapoint from './img/rikka-mad.gif';
+
+import {Link} from 'react-router-dom';
+
 import Troll from './components/trollPage/Troll';
 import Happy  from './components/happy/happy';
 import Hi from './components/message/hi';
@@ -20,9 +25,22 @@ import song from './img/bk.mp3';
 
 class App extends Component {
 
+  state = {
+    buttonDisplay : false
+  }
+
+
   playsond = () => {
     let x = document.getElementById("myAudio"); 
-    // x.play(); 
+    x.volume = .2;
+    x.play(); 
+
+
+    this.setState({
+      buttonDisplay: true
+    })
+
+
   }
 
   // componentDidMount = () => {
@@ -33,44 +51,53 @@ class App extends Component {
   //   console.log('un mount')
   // }
 
-  messageAlert = () => {
-  
-  }
+
   
 
   render() {
+
+    let homepage = (
+      <div class="main-container">   
+      <h1 className="title animation-right">Increase Your Volume!</h1>
+      <div className="flex">
+          <div className="flex1">
+          <Link  to="/main" className="btn btn--white animation" onClick={this.playsond}> Start <span>🤔</span></Link>
+          </div>
+          <div className="flex2">
+              <img className="animation-long" src={Rikkapoint} alt="rikka"/>
+          </div>
+      </div>
+  </div>
+    );
+
+    if(this.state.buttonDisplay){
+      homepage = (<div style={{display:"none"}}> sdf</div>);
+    }
     
     let routes = (
       <Switch>
-        <Route path="/" exact component={MainPage}/> 
+        <Route path="/main" exact component={MainPage}/> 
         <Route path="/troll"  exact component={Troll}/> 
         <Route path="/wow" component={Happy} />
         <Route path="/hi" component={Hi} />
         <Route path="/specialDay" component={SpecialDay} />
         <Route path="/celebrate" component={Celebrate} />
         <Route path="/wish" component={Wish} />
-
       </Switch>
     );
 
 
-    
     return (
-      <div className="App">
 
-        <audio id="myAudio"  autoPlay="autoplay" loop >
+      <div className="App main"> 
+        <audio id="myAudio" loop >
         <source src={song} type="audio/mpeg" >
         </source>
         </audio>
 
-        {this.messageAlert()}
-
-      
-  
-     
         {routes}
 
-    
+        {homepage}
       </div>
     );
   }
